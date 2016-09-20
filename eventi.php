d6b5072228cc46ed4b45f7f0a52f8d3a8cd83c09
BuildTools <unconfigured@null.spigotmc.org>
2016-09-20 16:12:16 +0200

    ...

--- eventi.php
+++ eventi.php
@@ -1,5 +1,61 @@
 <!DOCTYPE HTML>
 
+<?php
+
+	function listEvents() {
+
+		$out = array();
+
+		foreach(scandir("eventi/") as $dir) {
+			        
+			$realdir = "eventi/".$dir."/";
+			        
+			if (!is_dir($realdir) || strcmp($dir, ".") == 0 || strcmp($dir, "..") == 0){
+			            
+			    continue;
+			}
+			        
+			echo '<script>console.log("'. $realdir .'")</script>';
+
+    		array_push($out, $realdir);
+		}
+
+		return $out;
+	}
+
+	function createEventLink($page, $obj) {
+
+		echo '<a href="'. $page .'" id="'. $obj['name'] .'" class="eventlink">', PHP_EOL;
+		echo '    <img class="eventimg" src="'. $obj['img'] .'" alt="Foto evento"/>', PHP_EOL;
+		echo '    <h2 class="eventdate">'. $obj['date'] .'</h2>', PHP_EOL;
+		echo '    <h3 class="eventtitle">'. $obj['name'] .'</h3>', PHP_EOL;
+		echo '</a>', PHP_EOL;
+	}
+
+	function customEvents($dirlist) {
+
+		foreach ($dirlist as $dir) {
+
+			$config = fopen($dir . "config.json", "r") or die ("Unable to open file config.json");
+			$text = fread($config, filesize($dir . "config.json"));
+			fclose($config);
+
+			$obj = json_decode($text, true);
+
+			if (sizeof($obj) == 0) {
+
+				echo '<script>console.log("'. $dir .'config.json is empty")</script>', PHP_EOL;
+				return;
+			}
+
+			$page = $dir . "pagina.html";
+			$obj['img'] = $dir . $obj['img'];
+
+			createEventLink($page, $obj);
+		}
+	}
+?>
+
 <html>
 
 	<head>
@@ -9,28 +65,6 @@
 		<meta name="description" content="Sito ufficiale de La Magia Delle Corde">
 		<link title="style" media="screen" rel="stylesheet" href="style.css" type="text/css">
 
-		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-		<script type="text/javascript" src="js/events.js"> charset="UTF-8" </script>
-
-		<?php
-
-		function listEvents() {
-
-			$website = "/home/lele/Documenti/HtmlFiles/LaMagiaDelleCorde/";
-
-			$out = array();
-			$i = 0;
-
-			foreach(glob($website ."eventi", GLOB_ONLYDIR) as $dir) {
-
-    			array_push($out, (("key".strval($i)) => $dir));
-    			$i++;
-			}
-
-			return $out;
-		}
-		?>
-
 	</head>
 
 	<body>
@@ -39,11 +73,11 @@
 
 			<div class="menu">
 
-				<a class="link" id="link1" href="lascuola.html">La scuola</a>
-				<a class="link" id="link2" href="eventi.html">Eventi</a>
-				<a class="link" id="link3" href="iscrizione.html">Iscriviti</a>
+				<a class="link" id="link1" href="index.html">Homepage</a>
+				<a class="link" id="link2" href="contatti.html">Contatti</a>
+				<a class="link" id="link3" href="lascuola.html">La scuola</a>
 				<a class="link" id="link4" href="corsi.html">Corsi</a>
-				<a class="link" id="link5" href="contatti.html">Contatti</a>
+				<a class="link" id="link5" href="iscrizione.html">Iscriviti</a>
 
 			</div>
 
@@ -57,18 +91,13 @@
 
 				<h1 id="titolo"> Eventi </h1>
 
+				<?php customEvents(listEvents()); ?>
+
 			</div>
 
 		</div>
 
 		<script src="js/bounds.js"></script>
-
-		<script>
-
-			var dirs = <?php echo json_encode(listEvents()) ?>;
-			customEvents(jQuery.parseJSON(dirs));
-
-		</script>
 
 	</body>
 
