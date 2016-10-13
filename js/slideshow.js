2de61fac517e1a114238fb2b8254404ce6a9a533
BuildTools <unconfigured@null.spigotmc.org>
2016-10-13 10:39:32 +0200

    ..

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,38 +1,48 @@
 
-var namelist = new Array();
-var current = -1;
+var elements;
+var current;
 
-function switch(name) {
+function switch(index) {
+
+	if (index < 0 || index >= elements.length)
+		return;
 	
-
+	elements[current].style.display = 'none';
+	elements[index].style.display = 'block';
+	current = index;
 }
 
 function next() {
 
+	elements[current].style.display = 'none';
 
+	if ((++current) == elements.length)
+		current = 0;	// set first
+
+	elements[current].style.display = 'block';
 }
 
 function previous() {
 
+	elements[current].style.display = 'none';
 
+	if ((--current) < 0)
+		current = elements.length - 1;	// set last
+	
+	elements[current].style.display = 'block';
 }
 
 var timer = 5000; /* milliseconds */
-var timerManager = null;
 
-function enableSelfSwitch() {
+function selfSwitch() { 	// Call this function at the end of the document
 
-	if (timerManager)
-		return;
-
-	timerManager = setInterval(next, timer);
+	next();
+	setTimeout(selfSwitch, timer);	/* execute this function recursively */
 }
 
-function disableSelfSwitch() {
+(function() {
 
-	if (!timerManager)
-		return;
-
-	clearInterval(timerManager);
-	timerManager = null;
-}
+	elements = documents.getElementsByClassName('eventlink');
+	elements[0].style.display = 'block';
+	current = 0;
+})();
